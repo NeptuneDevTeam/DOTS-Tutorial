ab894a1bf94846f3ed9e4319146267ad4fed2024
will chapman <williamchapman53@gmail.com>
2024-12-31 11:49:17 -0500

    started a new project for Job tutorial

---
+++ Jobs-Tutorial/Assets/Scripts/Seeker.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Seeker : MonoBehaviour
+{
+    public Vector3 Direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += Direction * Time.deltaTime; 
+    }
+}
