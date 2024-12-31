256ae62c4472b5bb25ad402b8e31fe660780597e
will chapman <williamchapman53@gmail.com>
2024-12-31 17:17:20 -0500

    fixed FindNearestScript

--- Jobs-Tutorial/Assets/Scripts/FindNearest.cs
+++ Jobs-Tutorial/Assets/Scripts/FindNearest.cs
@@ -15,20 +15,29 @@ public class FindNearest : MonoBehaviour
     // avoids computing square roots.
     void Update()
     {
-        foreach (var seekerTransform in Spawner.SeekerTransforms) {
+        // Find the nearest Target.
+        // When comparing distances, it's cheaper to compare
+        // the squares of the distances because doing so
+        // avoids computing square roots.
+        foreach (var seekerTransform in Spawner.SeekerTransforms)
+        {
             Vector3 seekerPos = seekerTransform.localPosition;
             Vector3 nearestTargetPos = default;
             float nearestDistSq = float.MaxValue;
-            foreach (var targetTransform in Spawner.TargetTransforms) { 
-            Vector3 offset = targetTransform.localPosition - seekerPos;
-            float distSq = offset.sqrMagnitude;
-                if (distSq > nearestDistSq) {
+            foreach (var targetTransform in Spawner.TargetTransforms)
+            {
+                Vector3 offset = targetTransform.localPosition - seekerPos;
+                float distSq = offset.sqrMagnitude;
+
+                if (distSq < nearestDistSq)
+                {
                     nearestDistSq = distSq;
-                    nearestTargetPos  = targetTransform.localPosition;
+                    nearestTargetPos = targetTransform.localPosition;
                 }
             }
+
             Debug.DrawLine(seekerPos, nearestTargetPos);
-        }  
+        }
     }
    
 }
