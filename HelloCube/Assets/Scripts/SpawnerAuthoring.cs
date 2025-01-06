f9551d4418f41df683082a49751b448023bdc8cc
will chapman <williamchapman53@gmail.com>
2025-01-05 23:23:46 -0500

    commented out errors in Spawn System script

--- HelloCube/Assets/Scripts/SpawnerAuthoring.cs
+++ HelloCube/Assets/Scripts/SpawnerAuthoring.cs
@@ -1,8 +1,28 @@
+using Unity.Entities;
 using UnityEngine;
-
 public class SpawnerAuthoring : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject CubePrefab;
+
+    class Baker : Baker<SpawnerAuthoring> { 
+    public override void Bake(SpawnerAuthoring authoring)
+        {
+            var entity = GetEntity(authoring, TransformUsageFlags.None);
+            var spawner = new Spawner
+            {
+                CubePrefab = GetEntity(authoring.CubePrefab, TransformUsageFlags.Dynamic)
+            };
+            AddComponent(entity, spawner);
+        }
+    
+    }
+
+    struct Spawner : IComponentData {
+
+        public Entity CubePrefab;
+    }
     void Start()
     {
         
